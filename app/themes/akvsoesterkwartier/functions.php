d3e8054b133659c368d88b35c70cd80d40de46b6
Kees de Bruin <halfje.bruin@gmail.com>
2014-08-24 21:23:10 +0200

    Various style/layout changes using the Lynda.com tutorial

--- app/themes/akvsoesterkwartier/functions.php
+++ app/themes/akvsoesterkwartier/functions.php
@@ -1,72 +1,76 @@
 <?php
+
 /**
  * AKV Soesterkwartier functions and definitions
  *
  * @package AKV Soesterkwartier
  */
-
 /**
  * Set the content width based on the theme's design and stylesheet.
  */
-if ( ! isset( $content_width ) ) {
-	$content_width = 640; /* pixels */
+if ( !isset( $content_width ) )
+{
+    $content_width = 640; /* pixels */
 }
 
-if ( ! function_exists( 'akvs_setup' ) ) :
-/**
- * Sets up theme defaults and registers support for various WordPress features.
- *
- * Note that this function is hooked into the after_setup_theme hook, which
- * runs before the init hook. The init hook is too late for some features, such
- * as indicating support for post thumbnails.
- */
-function akvs_setup() {
+if ( !function_exists( 'akvs_setup' ) ) :
 
-	/*
-	 * Make theme available for translation.
-	 * Translations can be filed in the /languages/ directory.
-	 * If you're building a theme based on AKV Soesterkwartier, use a find and replace
-	 * to change 'akvs' to the name of your theme in all the template files
-	 */
-	load_theme_textdomain( 'akvs', get_template_directory() . '/languages' );
+    /**
+     * Sets up theme defaults and registers support for various WordPress features.
+     *
+     * Note that this function is hooked into the after_setup_theme hook, which
+     * runs before the init hook. The init hook is too late for some features, such
+     * as indicating support for post thumbnails.
+     */
+    function akvs_setup()
+    {
 
-	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
+        /*
+         * Make theme available for translation.
+         * Translations can be filed in the /languages/ directory.
+         * If you're building a theme based on AKV Soesterkwartier, use a find and replace
+         * to change 'akvs' to the name of your theme in all the template files
+         */
+        load_theme_textdomain( 'akvs', get_template_directory() . '/languages' );
 
-	/*
-	 * Enable support for Post Thumbnails on posts and pages.
-	 *
-	 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
-	 */
-	//add_theme_support( 'post-thumbnails' );
+        // Add default posts and comments RSS feed links to head.
+        add_theme_support( 'automatic-feed-links' );
 
-	// This theme uses wp_nav_menu() in one location.
-	register_nav_menus( array(
-		'primary' => __( 'Primary Menu', 'akvs' ),
-	) );
+        /*
+         * Enable support for Post Thumbnails on posts and pages.
+         *
+         * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
+         */
+        add_theme_support( 'post-thumbnails' );
 
-	/*
-	 * Switch default core markup for search form, comment form, and comments
-	 * to output valid HTML5.
-	 */
-	add_theme_support( 'html5', array(
-		'search-form', 'comment-form', 'comment-list', 'gallery', 'caption',
-	) );
+        // This theme uses wp_nav_menu() in one location.
+        register_nav_menus( array(
+            'primary' => __( 'Primary Menu', 'akvs' ),
+        ) );
 
-	/*
-	 * Enable support for Post Formats.
-	 * See http://codex.wordpress.org/Post_Formats
-	 */
-	add_theme_support( 'post-formats', array(
-		'aside', 'image', 'video', 'quote', 'link',
-	) );
+        /*
+         * Switch default core markup for search form, comment form, and comments
+         * to output valid HTML5.
+         */
+        add_theme_support( 'html5', array(
+            'search-form', 'comment-form', 'comment-list', 'gallery', 'caption',
+        ) );
 
-	// Setup the WordPress core custom background feature.
-	add_theme_support( 'custom-background', apply_filters( 'akvs_custom_background_args', array(
-		'default-color' => 'ffffff',
-		'default-image' => '',
-	) ) );
-}
+        /*
+         * Enable support for Post Formats.
+         * See http://codex.wordpress.org/Post_Formats
+         */
+        add_theme_support( 'post-formats', array(
+            'aside', 'image', 'video', 'quote', 'link',
+        ) );
+
+        // Setup the WordPress core custom background feature.
+        add_theme_support( 'custom-background', apply_filters( 'akvs_custom_background_args', array(
+            'default-color' => 'ffffff',
+            'default-image' => '',
+        ) ) );
+    }
+
 endif; // akvs_setup
 add_action( 'after_setup_theme', 'akvs_setup' );
 
@@ -75,39 +79,49 @@ add_action( 'after_setup_theme', 'akvs_setup' );
  *
  * @link http://codex.wordpress.org/Function_Reference/register_sidebar
  */
-function akvs_widgets_init() {
-	register_sidebar( array(
-		'name'          => __( 'Sidebar', 'akvs' ),
-		'id'            => 'sidebar-1',
-		'description'   => '',
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</aside>',
-		'before_title'  => '<h1 class="widget-title">',
-		'after_title'   => '</h1>',
-	) );
+function akvs_widgets_init()
+{
+    register_sidebar( array(
+        'name'          => __( 'Sidebar', 'akvs' ),
+        'id'            => 'sidebar-1',
+        'description'   => '',
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</aside>',
+        'before_title'  => '<h1 class="widget-title">',
+        'after_title'   => '</h1>',
+    ) );
 }
+
 add_action( 'widgets_init', 'akvs_widgets_init' );
 
 /**
  * Enqueue scripts and styles.
  */
-function akvs_scripts() {
-	wp_enqueue_style( 'akvs-style', get_stylesheet_uri() );
+function akvs_scripts()
+{
+    wp_enqueue_style( 'akvs-style', get_template_directory_uri() . '/css/akv-soesterkwartier.css' );
+    wp_enqueue_style( 'akvs-style-content-sidebar', get_template_directory_uri() . '/css/content-sidebar.css' );
 
-	wp_enqueue_script( 'akvs-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+    wp_enqueue_style( 'akvs-google-fonts', 'http://fonts.googleapis.com/css?family=Lato:100,400,700,900,400italic,900italic|PT+Serif:400,700,400italic,700italic' );
 
-	wp_enqueue_script( 'akvs-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+    wp_enqueue_style( 'akvs-fontawesome', 'http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css' );
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
+    wp_enqueue_script( 'akvs-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+
+    wp_enqueue_script( 'akvs-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+
+    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) )
+    {
+        wp_enqueue_script( 'comment-reply' );
+    }
 }
+
 add_action( 'wp_enqueue_scripts', 'akvs_scripts' );
 
 /**
  * Implement the Custom Header feature.
  */
-//require get_template_directory() . '/inc/custom-header.php';
+require get_template_directory() . '/inc/custom-header.php';
 
 /**
  * Custom template tags for this theme.
