5296b534fd4124b108f1b9a804030002135eff95
Kees de Bruin <halfje.bruin@gmail.com>
2014-08-29 11:45:13 +0200

    Added comment styling

--- app/themes/akvsoesterkwartier/comments.php
+++ app/themes/akvsoesterkwartier/comments.php
@@ -7,63 +7,51 @@
  *
  * @package AKV Soesterkwartier
  */
-
 /*
  * If the current post is protected by a password and
  * the visitor has not yet entered the password we will
  * return early without loading the comments.
  */
-if ( post_password_required() ) {
-	return;
+if (post_password_required()) {
+    return;
 }
 ?>
 
 <div id="comments" class="comments-area">
 
-	<?php // You can start editing here -- including this comment! ?>
-
-	<?php if ( have_comments() ) : ?>
-		<h2 class="comments-title">
-			<?php
-				printf( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'akvs' ),
-					number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
-			?>
-		</h2>
-
-		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
-		<nav id="comment-nav-above" class="comment-navigation" role="navigation">
-			<h1 class="screen-reader-text"><?php _e( 'Comment navigation', 'akvs' ); ?></h1>
-			<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'akvs' ) ); ?></div>
-			<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'akvs' ) ); ?></div>
-		</nav><!-- #comment-nav-above -->
-		<?php endif; // check for comment navigation ?>
-
-		<ol class="comment-list">
-			<?php
-				wp_list_comments( array(
-					'style'      => 'ol',
-					'short_ping' => true,
-				) );
-			?>
-		</ol><!-- .comment-list -->
-
-		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
-		<nav id="comment-nav-below" class="comment-navigation" role="navigation">
-			<h1 class="screen-reader-text"><?php _e( 'Comment navigation', 'akvs' ); ?></h1>
-			<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'akvs' ) ); ?></div>
-			<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'akvs' ) ); ?></div>
-		</nav><!-- #comment-nav-below -->
-		<?php endif; // check for comment navigation ?>
-
-	<?php endif; // have_comments() ?>
-
+    <?php if (have_comments()) : ?>
+    <h2 class="comments-title">
 	<?php
-		// If comments are closed and there are comments, let's leave a little note, shall we?
-		if ( ! comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
+	printf(_nx('One comment:', '%1$s comments:', get_comments_number(), 'comments title', 'akvs'), number_format_i18n(get_comments_number()));
 	?>
-		<p class="no-comments"><?php _e( 'Comments are closed.', 'akvs' ); ?></p>
-	<?php endif; ?>
+    </h2>
 
-	<?php comment_form(); ?>
+    <ol class="comment-list">
+    <?php
+    wp_list_comments(array(
+	'style' => 'ol',
+	'short_ping' => true,
+	'avatar_size' => 50,
+    ));
+    ?>
+    </ol><!-- .comment-list -->
+
+    <?php if (get_comment_pages_count() > 1 && get_option('page_comments')) : // are there comments to navigate through  ?>
+    <nav id="comment-nav-below" class="comment-navigation clear" role="navigation">
+	<h1 class="screen-reader-text"><?php _e('Comment navigation', 'akvs'); ?></h1>
+	<div class="nav-previous"><?php previous_comments_link(__('<i class="fa fa-arrow-circle-o-left"></i> Older Comments', 'akvs')); ?></div>
+	<div class="nav-next"><?php next_comments_link(__('Newer Comments <i class="fa fa-arrow-circle-o-right"></i>', 'akvs')); ?></div>
+    </nav><!-- #comment-nav-below -->
+    <?php endif; // check for comment navigation  ?>
+
+    <?php endif; // have_comments() ?>
+
+    <?php
+    // If comments are closed and there are comments, let's leave a little note, shall we?
+    if (!comments_open() && '0' != get_comments_number() && post_type_supports(get_post_type(), 'comments')) : ?>
+    <p class="no-comments"><?php _e('Comments are closed.', 'akvs'); ?></p>
+    <?php endif; ?>
+
+    <?php comment_form(); ?>
 
 </div><!-- #comments -->
