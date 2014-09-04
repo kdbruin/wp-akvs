98480cc0c7ea5b1f982d2d145a7d16eeb1c04524
Kees de Bruin <halfje.bruin@gmail.com>
2014-09-04 21:30:13 +0200

    Single post formattings and updated WP 4.0 language files

--- app/themes/akvs/inc/template-tags.php
+++ app/themes/akvs/inc/template-tags.php
@@ -75,7 +75,7 @@ if ( !function_exists( 'akvs_posted_on' ) ) :
      */
     function akvs_posted_on()
     {
-        $time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+        $time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
         if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) )
         {
             $time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
@@ -85,14 +85,14 @@ if ( !function_exists( 'akvs_posted_on' ) ) :
         );
 
         $posted_on = sprintf(
-            _x( 'Posted on %s', 'post date', 'akvs' ), '<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+            _x( '%s', 'post date', 'akvs' ), '<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
         );
 
         $byline = sprintf(
-            _x( 'by %s', 'post author', 'akvs' ), '<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+            _x( 'Written by %s', 'post author', 'akvs' ), '<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
         );
 
-        echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>';
+        echo '<span class="byline"> ' . $byline . '</span><span class="posted-on">' . $posted_on . '</span>';
     }
 
 endif;
